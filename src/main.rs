b517f88e7b90ee6a46d30f9e3e8e11f074e76248
Rasmus Winther <rasmus@flowplan.dk>
2024-03-16 16:54:32 +0100

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 use rodio::{source::Repeat, source::Source, Decoder, OutputStream, Sink};
+use rppal::gpio::Gpio;
 use std::fs::File;
 use std::io::BufReader;
-use rppal::gpio::Gpio;
 use std::{thread, time};
-
-enum State {
-    Open,
-    Close,
-}
 
 fn load_sound(name: String) -> Repeat<Decoder<BufReader<File>>> {
     println!("loading file");
-    let file = BufReader::new(File::open(name).unwrap());
-    // Decode that sound file into a source
-    println!("decoding file");
+    let file = BufReader::new(
+        File::open(&name).expect(&format!("No file called exist: {}", &name).to_owned()),
+    );
     Decoder::new(file).unwrap().repeat_infinite()
 }
 
@@ -25,20 +20,19 @@ fn main() {
     sink.play();
     sink.pause();
     let gpio = Gpio::new().unwrap();
-    let mut pin = gpio.get(23).unwrap().into_input_pullup();
-    let state: State = State::Open;
+    let pin = gpio.get(23).unwrap().into_input_pullup();
     let debounce = time::Duration::from_millis(10);
     loop {
         match (pin.is_high(), sink.is_paused()) {
             (true, true) => {
                 println!("starting file");
                 sink.play();
-            },
+            }
             (false, false) => {
                 println!("pause file");
                 sink.pause()
-            },
-            (_, _) => ()
+            }
+            (_, _) => (),
         }
         thread::sleep(debounce);
     }
